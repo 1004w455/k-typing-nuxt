125a17a3d5e126f3f8fcc1c4559f850e2372c47a
강상규 <2014ksg@adwitt.com>
2018-11-29 18:51:04 +0900

    init

---
+++ layouts/error.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">[{{ error.statusCode }}] {{ error.message }}</h1>
+    <h1 v-else>An error occurred</h1>
+    <nuxt-link to="/">Home page</nuxt-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Error',
+    props: {
+      error: {
+        type: Object,
+        default: function () {
+          return { statusCode: 0, message: '' }
+        }
+      },
+    },
+    layout: 'blog'
+  };
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/inspire.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-layout>
+    <v-flex text-xs-center>
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5">
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    validate (/*{ params }*/context) {
+      // Must be a number
+      //return /^\d+$/.test(params.id)
+      console.log(context);
+      return false;
+    }
+  }
+</script>
